951f86a27bce6cca38be2635bc7e0316c7431768
stardev96610 <stardev96610@gmail.com>
2022-03-17 14:32:42 +0900

    responive carousel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 //components
 import MyNavbar from "../../components/navbars/Navbar";
 import { Carousel } from "react-bootstrap";
-// images
+//Slider images
 import FirstSlide from '../../assets/images/slider1.png';
 import SecondSlide from '../../assets/images/slider2.png';
 import ThirdSlide from '../../assets/images/slider3.png';
-
+// News images
+import NewsImages from '../../assets/images/news.svg';
 //scss
 import "../../assets/scss/custom/homepage.scss";
 const Home = () => {
@@ -23,14 +24,14 @@ const Home = () => {
                     <Carousel.Item>
                         <img src={FirstSlide} alt="First Slide" className="d-block w-50" />
                         <Carousel.Caption>
-                            <h3 className="text-white">Start earning interest on crypto today</h3>
+                            <h3 className="text-white">Start Earning Interest On Crypto Today</h3>
                             <p>Trade a variety of digital assets like Bitcoin, Ethereum, Solana, and Dogecoin with low trading fees</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src={SecondSlide} alt="Second Slide" className="d-block w-50" />
                         <Carousel.Caption>
-                            <h3 className="text-white">Find the hottest assets in Diverge</h3>
+                            <h3 className="text-white">Find The Hottest Assets In Diverge</h3>
                             <p>A crypto wallet that anyone can get started in one minute</p>
                         </Carousel.Caption>
                     </Carousel.Item>
@@ -42,6 +43,10 @@ const Home = () => {
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel> 
+            </div>
+            {/* News Section */}
+            <div className="news_section bg-black text-center p-3 pt-2">
+                <img src={NewsImages} alt=""  className="w-100"/>
             </div>
             {/* hero */}
             {/* <Hero /> */}
